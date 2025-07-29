f89385ddf4b193b6b39aaceeb4e9326465001f47
annah <annah@gmail.com>
2025-07-30 01:22:36 +0300

    initial commit

---
+++ calculato.py
@@ -0,0 +1,12 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number; "))
+sum_result = num1+num2
+difference_result =num1-num2
+product_result= num1*num2
+quontiet_result = num1/num2
+
+print(f"Result of your two numbers; ")
+print(f"sum: {sum_result}")
+print(f"difference: {difference_result}")
+print(f"product: {product_result}")
+print(f"division: {quontiet_result}")
